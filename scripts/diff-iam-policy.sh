e78c1cbe1759cb190302dca807ef7f75543eb469
Josh Rowe <josh.rowe@digital.justice.gov.uk>
2019-06-13 11:35:16 +0100

    Call scripts from concourse yaml files

--- scripts/diff-iam-policy.sh
+++ scripts/diff-iam-policy.sh
@@ -1,12 +1,12 @@
 cat policies/list.json | jq -r '.[] | [.app_name, .role, (.policy|tostring)] | @tsv' |
   while IFS=$'\t' read -r app_name role policy; do
-    echo $app_name
-    echo $role
-    current=$(aws iam get-role-policy --role-name $role --policy-name $app_name | jq -r '.PolicyDocument')
-    echo $current | jq -S . > current.json
-    echo $policy | jq -S . > new.json
+    echo ${app_name}
+    echo ${role}
+    current=$(aws iam get-role-policy --role-name ${role} --policy-name ${app_name} | jq -r '.PolicyDocument')
+    echo ${current} | jq -S . > current.json
+    echo ${policy} | jq -S . > new.json
     export ARE_SAME=$(jq --argfile a new.json --argfile b current.json -n '$a==$b')
-    if [ \"$ARE_SAME\" != \"true\" ]; then
+    if [[ \"${ARE_SAME}\" != \"true\" ]]; then
       echo -e '\033[0;31mChange in iam_policy.json'
       diff current.json new.json
       exit 166
--- scripts/extract-policies-from-repos.sh
+++ scripts/extract-policies-from-repos.sh
@@ -6,17 +6,17 @@ cat repos/list.txt
 cat repos/list.txt | while read line
 do
   {
-    git clone https://moj-analytical-services:$GITHUB_TOKEN@github.com/moj-analytical-services/$line.git
-    if [ -f $line/iam_config.yaml ] && [ ! -f $line/iam_policy.json ]; then
+    git clone https://moj-analytical-services:${GITHUB_TOKEN}@github.com/moj-analytical-services/${line}.git
+    if [[ -f ${line}/iam_config.yaml ]] && [[ ! -f ${line}/iam_policy.json ]]; then
       echo 'Building iam_policy.json from iam_config.yaml'
-      iam_builder -c $line/iam_config.yaml -o $line/iam_policy.json
+      iam_builder -c ${line}/iam_config.yaml -o ${line}/iam_policy.json
     fi
-    if [ -f $line/deploy.json ] && [ -f $line/iam_policy.json ]; then
-      role=$(cat $line/deploy.json | jq -r '.role_name')
-      policy=$(cat $line/iam_policy.json | jq -r .)
-      rm -rf $line
+    if [[ -f ${line}/deploy.json ]] && [[ -f ${line}/iam_policy.json ]]; then
+      role=$(cat ${line}/deploy.json | jq -r '.role_name')
+      policy=$(cat ${line}/iam_policy.json | jq -r .)
+      rm -rf ${line}
       policies=$(cat policies/list.json | jq --argjson policy "$policy" --arg role "$role"  --arg app_name "$line" '. += [{role: $role, policy: $policy, app_name: $app_name}]')
-      echo $policies | jq -S . > policies/list.json
+      echo ${policies} | jq -S . > policies/list.json
     fi
   } || {
     echo 'No repo'
