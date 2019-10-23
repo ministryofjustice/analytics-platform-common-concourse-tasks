4b998f45d6ebcc4b4388ba398785da7a92b02584
Josh Rowe <josh@joshs-mbp-2018.ws.local>
2019-10-23 16:43:07 +0100

    Add secret env vars from AWS Paramater store

--- scripts/parse_deploy_file.py
+++ scripts/parse_deploy_file.py
@@ -28,6 +28,15 @@ webapp_port = data.get('port', 80)
 
 health_check = data.get('health_check', '/?healthz')
 
+ssm = boto3.client('ssm')
+role_name = data.get('role_name')
+parameter_path = f'/alpha/webapp/{role_name}/secrets/'
+parameters = ssm.get_parameters_by_path(
+  Path=parameter_path,
+  WithDecryption=True,
+).get('Parameters', [])
+env_vars = {p['Name']: p['Value'] for p in parameters}
+
 # The following are input values of the webapp helm chart
 with open('deploy-params/overrides.yaml', 'w') as output:
     json.dump({
@@ -39,5 +48,6 @@ with open('deploy-params/overrides.yaml', 'w') as output:
             'Port': webapp_port,
             'HealthCheck': health_check,
         },
+        'secretEnv': env_vars
     }, output)
 print('Wrote deploy-params/overrides.yaml')
